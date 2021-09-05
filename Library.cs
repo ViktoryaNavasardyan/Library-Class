d8556d4d8528026beb4038b2a6d623e1450e6d65
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-05 19:16:05 +0400

    Add files via upload

---
+++ Library.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_class.Folder
+{
+    class Library
+    {
+        public static string openingTime = "9:30";
+        public static string closingTime = "18:30";
+        protected List<Section> section;
+        public List<Section> GetSection { get => section; }
+        public Library() { section = new List<Section>(); }
+        public Library(List<Section> sections) : this()
+        {
+            for (int i = 0; i < sections.Count(); i++)
+            {
+                section.Add(sections[i]);
+            }
+        }
+        public int FindIndexOfSection(Section searchedSection)
+        {
+            int index = -1;
+            for (int i = 0; i < section.Count(); i++)
+            {
+                if (searchedSection.Name == section[i].Name)
+                    index = i;
+            }
+            return index;
+        }
+        //find by name
+        public int FindIndexOfSection(string name)
+        {
+            int index = -1;
+            for (int i = 0; i < section.Count(); i++)
+            {
+                if (section[i].Name == name)
+                    index = i;
+            }
+            return index;
+        }
+        //search section
+        public bool SearchSection(string name, out Section finalSection)
+        {
+            for (int i = 0; i < section.Count(); i++)
+            {
+                if (section[i].Name == name)
+                {
+                    finalSection = section[i];
+                    return true;
+                }
+            }
+            //The section doesn't include this book.
+            finalSection = null;
+            return false;
+        }
+        //search available books
+        public List<Book> SearchAvailableBooks()
+        {
+            List<Book> result = new List<Book>();
+            for (int i = 0; i < section.Count(); i++)
+            {
+                for (int j = 0; j < section[i].GetBookCount; j++)
+                {
+                    if (section[i].Book[j]._isAvailable)
+                    {
+                        result.Add(section[i].Book[j]);
+                    }
+                }
+            }
+            return result;
+        }
+        //getAllBooks
+        public List<Book> GetAllBooks()
+        {
+            List<Book> result = new List<Book>();
+            for (int i = 0; i < section.Count(); i++)
+            {
+                for (int j = 0; j < section[i].GetBookCount; j++)
+                {
+                        result.Add(section[i].Book[j]);
+                }
+            }
+            return result;
+        }
+        //Get Section Books by section name
+        public List<Book> GetSectionBooksBySectionName(string sectionName)
+        {
+            List<Book> result = new List<Book>();
+            for (int i = 0; i < section.Count(); i++)
+            {
+                for (int j = 0; j < section[i].GetBookCount; j++)
+                {
+                    if (section[i].Name == sectionName)
+                        result.Add(section[i].Book[j]);
+                }
+            }
+            return result;
+        }
+        //Get Books By Genre
+        public List<Book> GetBooksByGenre(string genre)
+        {
+            List<Book> result = new List<Book>();
+            for (int i = 0; i < section.Count(); i++)
+            {
+                for (int j = 0; j < section[i].GetBookCount; j++)
+                {
+                    if (section[i].Book[j].Genre == genre)
+                        result.Add(section[i].Book[j]);
+                }
+            }
+            return result;
+        }
+        public bool SearchBook(Book givingBook, out Book result)
+        {
+            result = null;
+            for (int i = 0; i < section.Count(); i++)
+            {
+                for (int j = 0; j < section[i].GetBookCount; j++)
+                {
+                    if (section[i].SearchBook(givingBook.Name, out result))
+                        return true;
+                }
+            }
+            //The library doesn't include this book.
+            return false;
+        }
+        //Search book by name
+        public bool SearchBook(string name, out Book result)
+        {
+            result = null;
+            for (int i = 0; i < section.Count(); i++)
+            {
+                for (int j = 0; j < section[i].GetBookCount; j++)
+                {
+                    if (section[i].SearchBook(name, out result))
+                        return true;
+                }
+            }
+            //The library doesn't include this book.
+            return false;
+        }
+        public void AddSection(string sectionName)
+        {
+            Section addedSection = new Section(sectionName);
+            section.Add(addedSection);
+        }
+        public void AddSection(Section s)
+        {
+            section.Add(s);
+        }
+        public bool RemoveSection(string sectionName)
+        {
+            int resultIndex = FindIndexOfSection(sectionName);
+            if ( resultIndex == -1)
+            {
+                return false;
+            }
+            section.Remove(section[resultIndex]);
+            return true;
+            
+        }
+        public void ShowLibrary()
+        {
+            Console.WriteLine("In this library there are this sections ->");
+            for(int i = 0; i < section.Count(); i++)
+            {
+                section[i].ShowSection();
+            Console.WriteLine("==============================================================================");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using library_class.Folder;
+
+
+namespace library_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Library openning and closing time.
+            Console.WriteLine($"Our libraries open at {Library.openingTime} and close at {Library.closingTime}.");
+            BooksSectionsLibrary();
+            SearchRemove();
+            FindAndGiveBooks();
+            
+        }
+        //Adding books and section into Library 1
+        static void BooksSectionsLibrary()
+        {
+            Book book1 = new Book("Life of Pi", "Yann Martel", "Action", 326);
+            Book book2 = new Book("Little Women", "Louisa May Alcott", "Classics", 528);
+            Book book3 = new Book("The Adventures of Sherlock Holmes", "Sir Arthur Conan Doyle", "Detective", 164);
+            Book book4 = new Book("Bird Box", "Josh Malerman", "Horror", 305);
+            Book book5 = new Book("Harry Potter and the Order of the Phoenix", "Joanne Rowling", "Action", 766);
+            Book book6 = new Book("1984", "George Orwell", "Classics", 320);
+            Book book7 = new Book("Pride and Prejudice", "Jane Austen", "Classecs", 384);
+            Book book8 = new Book("The Cat in the Hat", "Theodor Seuss Geisel", "Story", 61);
+            Book book9 = new Book("Charlie and the Chocolate Factory", "Roald Dahl", "Novel", 30);
+            Book book10 = new Book("Wonder", "Raquel Jaramillo Palacio", "Novel", 310);
+            List<Book> ForAdults = new List<Book>() { book1, book2, book3, book4, book5, book6, book7 };
+            List<Book> ForKids = new List<Book>() { book8, book9, book10 };
+            Section Adults = new Section(ForAdults, "Adults");
+            Section Kids = new Section(ForKids, "Kids");
+            List<Section> MySections = new List<Section>() { Adults, Kids };
+            Library MyLibrary = new Library(MySections);
+            MyLibrary.ShowLibrary();
+        }
+
+        //Creating sections and adding sections and books into library 2.
+        //Search some book, remove it, after that repead searching. Removing the section
+        static void SearchRemove()
+        {
+            Library MySecondLibrary = new Library();
+            //Create new section by name "study".
+            Section Study = new Section("Study");
+            //create book
+            Book book = new Book("Grock algorithms", "Aditya Bhargava", "Programming", 500);
+            //add the book into the section
+            Study.AddBook(book);
+            //add another book into the same section 
+            Study.AddBook("Linar Algebra", "Aleqsanyan", "Algebra", 450);
+            //add section into library
+            MySecondLibrary.AddSection(Study);
+
+            //Show "Study" section
+            //MySecondLibrary.GetSection[MySecondLibrary.FindIndexOfSection("Study")].ShowSection();
+
+            //Add new section by name
+            MySecondLibrary.AddSection("Tests");
+            //Add book into library in section Tests.
+            MySecondLibrary.GetSection[MySecondLibrary.FindIndexOfSection("Tests")].AddBook("Harcashar", 3);
+
+            //Show "Tests" Section
+            //MySecondLibrary.GetSection[MySecondLibrary.FindIndexOfSection("Tests")].ShowSection();
+
+            //Print My Library
+            MySecondLibrary.ShowLibrary();
+
+            //Search some book, remove it, after that repead searching
+            Book resultBook = new Book();
+            if (MySecondLibrary.GetSection[MySecondLibrary.FindIndexOfSection("Tests")].SearchBook("Harcashar", out resultBook))
+            {
+                Console.WriteLine("Here is searched book->");
+                resultBook.ShowBook();
+                Console.WriteLine("We find the book, so now we can remove it.");
+                MySecondLibrary.GetSection[MySecondLibrary.FindIndexOfSection("Tests")].RemoveBook("Harcashar", null);
+                Console.WriteLine("Repead searching to make sure that the book was removed.");
+                if (!MySecondLibrary.GetSection[MySecondLibrary.FindIndexOfSection("Tests")].SearchBook("Harcashar", out resultBook))
+                {
+                    Console.WriteLine("The book was removed.");
+                    Console.WriteLine();
+                }
+                else
+                    Console.WriteLine("Removing is not succeed.");
+            }
+            else
+                Console.WriteLine("The book was not found.");
+            //trying remove section "Tests"
+            Section resultSection = new Section();
+            if (MySecondLibrary.SearchSection("Tests", out resultSection))
+            {
+                Console.WriteLine("Here is searched section->");
+                resultSection.ShowSection();
+                Console.WriteLine("We find the section, so now we can remove it.");
+                MySecondLibrary.RemoveSection("Tests");
+                Console.WriteLine("Repead searching to make sure that the section was removed.");
+                if (!MySecondLibrary.SearchSection("Tests", out resultSection))
+                {
+                    Console.WriteLine("The section was removed.");
+                }
+                else
+                    Console.WriteLine("Removing is not succeed.");
+            }
+            else
+                Console.WriteLine("The section was not found.");
+
+        }
+
+        //Add books to the library 3, find books by giving genre, giving to the person,checking available books
+        //and all books after giving
+        static void FindAndGiveBooks()
+        {
+            Library MyThirdLibrary = new Library();
+            MyThirdLibrary.AddSection("English");
+            MyThirdLibrary.AddSection("Russian");
+            MyThirdLibrary.AddSection("Armenian");
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("English")].AddBook("Engish First Class", "School", "First Class", 58);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("English")].AddBook("English Second Class", "School", "Second Class", 50);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("English")].AddBook("English Third Class", "School", "Third Class", 70);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("Russian")].AddBook("Russian First Class", "School", "First Class", 40);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("Russian")].AddBook("Russian Second Class", "School", "Second Class", 57);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("Russian")].AddBook("Russian Third Class", "School", "Third Class", 60);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("Armenian")].AddBook("Armenian First Class", "School", "First Class", 42);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("Armenian")].AddBook("Armenian Second Class", "School", "Second Class", 37);
+            MyThirdLibrary.GetSection[MyThirdLibrary.FindIndexOfSection("Armenian")].AddBook("Armenian Third Class", "School", "Third Class", 64);
+            MyThirdLibrary.ShowLibrary();
+            //Someone wanted to find books of first class.
+            List<Book> FirstClassBooks = new List<Book>();
+            FirstClassBooks = MyThirdLibrary.GetBooksByGenre("First Class");
+            Console.WriteLine("Books of First Class ->");
+            foreach (Book books in FirstClassBooks)
+            {
+                books.ShowBook();
+                Console.WriteLine("------------");
+            }
+            //now the person want to take the books of first class
+            Console.WriteLine("We give books of first class to person.----");
+            for (int i = 0; i < MyThirdLibrary.GetSection.Count; i++)
+            {
+                int index = MyThirdLibrary.GetSection[i].FindIndexOfBookByGenre("First Class");
+                if (index == -1)
+                {
+                    Console.WriteLine("The book was not found.");
+                }
+                else
+                    MyThirdLibrary.GetSection[i].GiveBookToTheCustomer(index);
+            }
+            //checking available books
+            Console.WriteLine("After giving some books to person, let's check available books.");
+            List<Book> AvailableBooks = new List<Book>();
+            for (int i = 0; i < MyThirdLibrary.GetSection.Count; i++)
+            {
+                AvailableBooks = MyThirdLibrary.SearchAvailableBooks();
+            }
+            foreach (Book books in AvailableBooks)
+            {
+                books.ShowBook();
+                Console.WriteLine("------------");
+            }
+            Console.WriteLine("After giving some books to person, let's check all books.");
+            List<Book> AllBooks = new List<Book>();
+            for (int i = 0; i < MyThirdLibrary.GetSection.Count; i++)
+            {
+                AllBooks = MyThirdLibrary.GetAllBooks();
+            }
+            foreach (Book books in AllBooks)
+            {
+                books.ShowBook();
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
