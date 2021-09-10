8a94acec3e553880df30e3d61332e3ee65191211
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-10 21:53:28 +0400

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,38 +13,25 @@ namespace library_class.Folder
         protected string _genre;
         protected int _pages;
         public bool _isAvailable;
-        public Book()
-        {
-            _pages = 0;
-            _isAvailable = false;
+        public Book() : this(null, null, null, 0, false)
+        {   
         }
-        public Book(string name, string author, string genre, int pages)
+        public Book(string name, string author, string genre, int pages, bool isAvailable = true)
         {
             _name = name;
             _author = author;
             _genre = genre;
             _pages = pages;
-            _isAvailable = true;
+            _isAvailable = isAvailable;
         }
-        public Book(string name, string author, string genre)
+        public Book(string name, string author, string genre) : this(name, author, genre, 0)
         {
-            _name = name;
-            _author = author;
-            _genre = genre;
-            _isAvailable = true;
         }
-        public Book(string name, string author, int pages)
+        public Book(string name, string author, int pages) : this(name, author, null, pages)
         {
-            _name = name;
-            _author = author;
-            _pages = pages;
-            _isAvailable = true;
         }
-        public Book(string name, int pages)
+        public Book(string name, int pages) : this(name, null, null, pages)
         {
-            _name = name;
-            _pages = pages;
-            _isAvailable = true;
         }
         public string Name{   get => _name; set => _name = value; }
         public string Author { get => _author; set => _author = value; }
